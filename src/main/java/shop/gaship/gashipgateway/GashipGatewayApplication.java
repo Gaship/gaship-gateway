baa5f56704cbae9f44c255f657b54136c43a9901
oct-sky-out <kms3335k@naver.com>
2022-07-07 00:00:08 +0900

    first 👑 : gaship-gateway 프로젝트 생성

---
+++ src/main/java/shop/gaship/gashipgateway/GashipGatewayApplication.java
@@ -0,0 +1,13 @@
+package shop.gaship.gashipgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GashipGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GashipGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/gaship/gashipgateway/GashipGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.gaship.gashipgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GashipGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
